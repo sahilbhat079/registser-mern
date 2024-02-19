ad046352948021c9c3fe9350bb60d0f0b245a405
sahilbhat079 <sahilgani.sahilgani@gmail.com>
2024-02-19 15:31:08 +0530

    something

--- loginsignup/src/components/Signup.jsx
+++ loginsignup/src/components/Signup.jsx
@@ -15,7 +15,7 @@ export default function Signup() {
     function handlesubmit(e){
         e.preventDefault();
         console.log({name,email,password});
-        axios.post('https://registser-mern-api.vercel.app',{name,email,password} ).then((res)=>{
+        axios.post('https://registser-mern-api.vercel.app/register',{name,email,password} ).then((res)=>{
             console.log(res)
             navigate( "/login");
 
